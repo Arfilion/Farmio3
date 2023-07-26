3c6a0733ea955b9ee1ff9b5e5dd4b9baf637b4d0
Arfilion <105883987+Arfilion@users.noreply.github.com>
2023-07-25 22:45:04 -0300

    Generic Boss

--- Assets/boss.cs
+++ Assets/boss.cs
@@ -14,15 +14,26 @@ public class boss : MonoBehaviour
     public Transform player;
     public float rotationSpeed = 2f;
     [SerializeField] Animator bossAnim;
+    public List<GameObject> prefabList = new List<GameObject>();
+    [SerializeField] ListGenerator<Center> centerList = new ListGenerator<Center>();
+    [SerializeField] ListGenerator<Side> sideList = new ListGenerator<Side>();
+    [SerializeField] ListGenerator<Roar> roarList = new ListGenerator<Roar>();
     public void Awake()
     {
-        durationRoar=11f;
+        durationRoar = 11f;
         durationInside = 6.66f;
         durationOutside = 7f;
         casting = false;
     }
     public void Start()
     {
+
+        roarList.Gatherer();
+        sideList.Gatherer();
+        centerList.Gatherer();
+
+
+
         // Crear un diccionario para almacenar habilidades
         Dictionary<string, Action> skillDictionary = new Dictionary<string, Action>();
 
@@ -44,7 +55,7 @@ public class boss : MonoBehaviour
         SideRay();
         InsideRay();
         StateChecker(state);
-       
+
     }
 
     private void RotationUpdate()
@@ -60,33 +71,36 @@ public class boss : MonoBehaviour
     }
 
     // Métodos que representan las habilidades
-    private  void Roar()
+    private void Roar()
     {
-         if (Input.GetKeyDown(KeyCode.M))
-         {
+        if (Input.GetKeyDown(KeyCode.M))
+        {
             counter = 0;
             counter += Time.deltaTime;
             casting = true;
             state = durationRoar;
             bossAnim.SetTrigger("Roar");
-            print("roar");
-       
-         }
+            StartCoroutine(roarList.ActivateColliders());
+
+
+
+        }
     }
-    private  void SideRay()
+    private void SideRay()
     {
         if (Input.GetKeyDown(KeyCode.B))
         {
             counter = 0;
             counter += Time.deltaTime;
             casting = true;
-
             state = durationOutside;
             bossAnim.SetTrigger("SideRay");
-            print("outside");
+            StartCoroutine(sideList.ActivateColliders());
+
+
         }
     }
-    private  void InsideRay()
+    private void InsideRay()
     {
         if (Input.GetKeyDown(KeyCode.N))
         {
@@ -95,7 +109,9 @@ public class boss : MonoBehaviour
             casting = true;
             state = durationInside;
             bossAnim.SetTrigger("InsideRay");
-            print("inside");
+            StartCoroutine(centerList.ActivateColliders());
+
+
         }
     }
     public void StateChecker(float _state)
@@ -107,5 +123,44 @@ public class boss : MonoBehaviour
             counter += Time.deltaTime;
         }
     }
-   
+  
 }
+public class ListGenerator<T> where T : MonoBehaviour
+{
+    public List<T> hitboxes = new List<T>();
+    public void Gatherer()
+    {
+        T[] actualHitboxes = UnityEngine.Object.FindObjectsOfType<T>();
+        hitboxes.AddRange(actualHitboxes);
+    }
+
+    public List<T> GetList()
+    {
+        return hitboxes;
+    }
+    
+    public IEnumerator ActivateColliders()
+    {
+        foreach (T hitbox in hitboxes)
+        {
+            Collider collider = hitbox.GetComponent<Collider>();
+            if (collider != null)
+            {
+                collider.enabled = true;
+            }
+        }
+        yield return new WaitForSeconds(3f);
+
+        foreach (T hitbox in hitboxes)
+        {
+            Collider collider = hitbox.GetComponent<Collider>();
+            if (collider != null)
+            {
+                collider.enabled = false;
+            }
+            Debug.Log("holi");
+        }
+    }
+  
+}
+
